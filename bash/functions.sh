e788fed1be2571a9bd3bf9f77e7027a737b7bf9b
Luciano Panepucci <lpanebr@gmail.com>
2013-08-06 10:41:08 -0300

    added autoload of bash source files to bashrc

---
+++ bash/functions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# my bash functions
+
+function cdsubmit () {
+   cd $MYSITES/submit/public/$1
+   guake -r Submit
+}
+
+function cdhost () {
+   cd $MYSITES/host/public/$1
+   guake -r Host
+}
+
+function cdportais () {
+   cd $MYSITES/portais/public/$1
+   guake -r Portais
+}
+
+function cddotfiles () {
+   cd /home/$USER/.dotfiles
+   guake -r dotfiles
+}
+
+function goturing () {
+   guake -r turing
+   ssh lpanebr@192.168.1.10
+}
+
+function gogutenberg () {
+   guake -r gutenberg
+   ssh lpanebr@192.168.1.8
+}
