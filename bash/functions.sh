9e4743a3fa33838e01a60f0ced533f3c60d86216
Alberto C. Alvarez <alberto.alvarez81@gmail.com>
2021-06-16 22:49:06 -0300

    initial cleanup

--- bash/functions.sh
+++ bash/functions.sh
@@ -2,51 +2,6 @@
 
 # my bash functions
 
-function music () {
-   guake -r cmus
-   cmus
-}
-
-function cdsubmit () {
-   cd $MYSITES/submit/public/$1
-   guake -r "Submit $1"
-}
-
-function cdhost () {
-   cd $MYSITES/host/public/$1
-   guake -r "Host $1"
-}
-
-function cdportais () {
-   cd $MYSITES/portais/public/$1
-   guake -r "Portais $1"
-}
-
-function cddotfiles () {
-   cd /home/$USER/.dotfiles
-   guake -r dotfiles
-}
-
-function cdcpub () {
-   cd $MYSITES/cpub/public/$1
-   guake -r cpub
-}
-
-function cdwebdev () {
-   cd $MYSITES/webdev/public/$1
-   guake -r webdev
-}
-
-function goturing () {
-   guake -r turing
-   ssh lpanebr@192.168.1.10
-}
-
-function gogutenberg () {
-   guake -r gutenberg
-   ssh lpanebr@192.168.1.8
-}
-
 function findhere () {
    grep -irn "$@" --exclude-dir='.git' .
 }
--- php/php-functions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# my php bash functions
-
-function createsite() {
-   sudo php /home/lpanebr/bin/lgp-create-project.php $@
-}
